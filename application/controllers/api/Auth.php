9dc6788515b6f55d23cb86fde4d98d845f2e2b06
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-05-23 13:20:52 +0700

    added Auth/login API

--- application/controllers/api/Auth.php
+++ application/controllers/api/Auth.php
@@ -24,11 +24,34 @@ class Auth extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('users_model');
 	}
 
-	public function index()
+	public function login()
 	{
-		// 
+		$email = $this->input->post('email');
+		$password = $this->input->post('password');
+		$validate = $this->users_model->login($email);
+		if ($validate != null) {
+			if (password_verify($password, $validate['password'])) {
+				$response = [
+					'code' => 200
+				];
+				echo json_encode($response);
+			} else {
+				$response = [
+					'code' => 404,
+					'message' => 'Password salah'
+				];
+				echo json_encode($response);
+			}
+		} else {
+			$response = [
+				'code' => 404,
+				'message' => 'Email belum terdaftar'
+			];
+			echo json_encode($response);
+		}
 	}
 }
 
---
+++ application/models/Users_model.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ *
+ * Model Users_model
+ *
+ * This Model for ...
+ * 
+ * @package		CodeIgniter
+ * @category	Model
+ * @author    Setiawan Jodi <jodisetiawan@fisip-untirta.ac.id>
+ * @link      https://github.com/setdjod/myci-extension/
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+class Users_model extends CI_Model
+{
+
+	// ------------------------------------------------------------------------
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function login($email)
+	{
+		$this->db->select('*');
+		$this->db->from('users');
+		$this->db->where('email', $email);
+		return $this->db->get()->row_array();
+	}
+}
+
+/* End of file Users_model.php */
+/* Location: ./application/models/Users_model.php */
