c3e09712906fb1d15d4d3716f8f27b1ac535e321
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-05-23 11:13:32 +0700

    config backEnd

---
+++ application/controllers/api/Auth.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+/**
+ *
+ * Controller Auth
+ *
+ * This controller for ...
+ *
+ * @package   CodeIgniter
+ * @category  Controller CI
+ * @author    Setiawan Jodi <jodisetiawan@fisip-untirta.ac.id>
+ * @author    Raul Guerrero <r.g.c@me.com>
+ * @link      https://github.com/setdjod/myci-extension/
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+class Auth extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		// 
+	}
+}
+
+
+/* End of file Auth.php */
+/* Location: ./application/controllers/Auth.php */
